ce8d78ea3d2aecb87716edb84b729006e073e145
jjlin133 <63435164+jjlin133@users.noreply.github.com>
2021-03-26 21:58:14 +0800

    Update func.py

--- module/func.py
+++ module/func.py
@@ -30,10 +30,10 @@ def sendTWder(event, mtext):
         if not money == '':  #匯率類幣別存在
             if money in keys:
 		rate_date = twder.now(currencies[money])[0]
-                date_message = str(rate_date) + '_'+ money 
+                date_message = str(rate_date) + '_' 
 		line_bot_api.reply_message(event.reply_token, TextSendMessage(text=date_message))
                 rate3 = float(twder.now(currencies[money])[3])  #由匯率套件取得匯率
-                message = date_message + '_即期買入匯率 : ' + str(rate3)+ '_(台灣銀行端) '
+                message = date_message + money + '_即期買入匯率 : ' + str(rate3)+ '_(台灣銀行端) '
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
             else:
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text='無此幣別匯率資料！'))
