1edf8ddebe484fb3d5283ad72e598a979007eb1d
jjlin133 <63435164+jjlin133@users.noreply.github.com>
2021-03-26 21:04:34 +0800

    Update urls.py

--- currency/urls.py
+++ currency/urls.py
@@ -1,18 +1,3 @@
-"""firstproject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
@@ -23,8 +8,11 @@ from myapp.views import sayhello,hello3,hello4,fv,fv2
 from django.conf import settings
 from django.conf.urls.static import static
 
+# 2021.0326 add views from linebotLUIS\luisapi\views 
+from myapp import views
 
 urlpatterns = [
+    url('^callback', views.callback),
     path('admin/', admin.site.urls),
     url(r'^$', sayhello), 
     url(r'^hello3/(\w+)/$', hello3), 
