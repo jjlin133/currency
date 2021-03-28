e14f899e6f5d4ddab4a7b6ad5d862e9ccaca8877
jjlin133 <63435164+jjlin133@users.noreply.github.com>
2021-03-28 15:52:06 +0800

    Update func.py

--- module/func.py
+++ module/func.py
@@ -80,3 +80,16 @@ def neuWeb(event):  #網頁連結
         line_bot_api.reply_message(event.reply_token,message)
     except:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def finWeb(event):  #網頁連結
+    try:
+        text1 ='''
+零存整付試算   https://currency2021.herokuapp.com/fv
+              '''
+        message = TextSendMessage(
+            text = text1
+#            text = 'https://liff.line.me/1654001451-0YAYOpyD'
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
