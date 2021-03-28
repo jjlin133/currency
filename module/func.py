30caadce9e77930e07a1b6e98284fdc97660ea7d
jjlin133 <63435164+jjlin133@users.noreply.github.com>
2021-03-28 14:21:45 +0800

    Update func.py

--- module/func.py
+++ module/func.py
@@ -37,18 +37,22 @@ def sendTWder(event, mtext):
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
             else:
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text='無此幣別匯率資料！'))
-        else:  #其他未知輸入
-            text = '無法了解你的意思，請重新輸入！'
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text))
-            
+#        else:  #其他未知輸入
+#            text = '無法了解你的意思，請重新輸入！'
+#            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text))            
     except:
-       line_bot_api.reply_message(event.reply_token, TextSendMessage(text='執行時產生錯誤！'))
+       text = '無法了解你的意思，請重新輸入！'
+       line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text))
 
 def neuWeb(event):  #網頁連結
     try:
         text1 ='''
 北歐福利網頁：https://kknews.cc/zh-tw/world/3q2r8ng.html
+
 銘傳網網頁：http://172.104.79.148/mcu/?act=shopping&cmd=main&pg_id=2020093000006
+
+零存整付試算網：https://currency2021.herokuapp.com/fv2
+
                '''
         message = TextSendMessage(
             text = text1
