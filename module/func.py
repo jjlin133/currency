43f8f24df4f77791174a3c3b3fd45c21d682ee8b
jjlin133 <63435164+jjlin133@users.noreply.github.com>
2021-04-07 09:04:44 +0800

    Update func.py

--- module/func.py
+++ module/func.py
@@ -28,9 +28,18 @@ def sendTWder(event, mtext):
         money = mtext       
         if not money == '':  #匯率類幣別存在
             if money in keys:
-                rate_date = twder.now(currencies[money])[0]  
-                rate3 = float(twder.now(currencies[money])[3])  #由匯率套件取得匯率
-                message =  rate_date + '\n' + money + '_即期買入匯率 \n ' + str(rate3)+ '_(台灣銀行端) '
+                rate_date = twder.now(currencies[money])[0]
+                cashBuy = float(twder.now(currencies[mtext])[1])  #現金買入_匯率
+                cashSell = float(twder.now(currencies[mtext])[2])  #現金賣出_匯率
+                checkBuy = float(twder.now(currencies[mtext])[3])  #即期買入_匯率
+                checkSell = float(twder.now(currencies[mtext])[4])  #即期賣出_匯率
+                message = rate_date + '\n' + mtext + '_台灣銀行端匯率'
+                message = message + '\n 現金買入 : ' + str(cashBuy)
+                message = message + '\n 現金賣出 : ' + str(cashSell)
+	              message = message + '\n 即期買入 : ' + str(checkBuy)
+                message = message + '\n 即期賣出 : ' + str(checkSell)
+#                rate3 = float(twder.now(currencies[money])[3])  #由匯率套件取得匯率
+#                message =  rate_date + '\n' + money + '_即期買入匯率 \n ' + str(rate3)+ '_(台灣銀行端) '
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
             else:
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text='無此幣別匯率資料！'))
